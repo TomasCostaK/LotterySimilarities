59303d51198cad678fb0061cd87d673ace78622b
Macbook <Macbook@aluno-2436.wireless.ua.pt>
2018-12-05 14:50:26 +0000

    retirados os .m

--- initialize.m
+++
@@ -1,4 +0,0 @@
-%Especificar tamanho com n
-function bloom = initialize(n)
-	bloom = zeros(1,n,"uint8");
-endfunction
--- string2hash.m
+++
@@ -1,8 +0,0 @@
-%Funçao usada no guiao anterior
-
-function hash = string2hash(str)
-  	hash = 5381*ones(size(str,1),1);
-	for i=1:size(str,2)
-    	hash = mod(hash*33 + str(:,i), 2^31 -1);
-  	end
-endfunction
